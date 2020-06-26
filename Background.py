0824895f246f609ba85c0455a22b1db15a4e1282
Seth <seth5124@Gmail.com>
2020-06-25 20:34:09 -0400

    Finished homeland and parents

--- Background.py
+++ Background.py
@@ -15,22 +15,21 @@ def roll(die_type, modifier=0):
 
 def roll_background(race=test_race):
     print(roll_homeland(race))
+    print(roll_parents(race))
 
 
 def roll_homeland(race=test_race):
-    homeland = races.find('%s/Homeland' % race)
-    die_type = homeland.attrib['dieType']
+    homeland_table = races.find('%s/Homeland' % race)
+    die_type = homeland_table.attrib['dieType']
     roll_value = roll(die_type)
-    homeland_return_string = "You Rolled: " + str(roll_value) + " Your homeland is: "
+    homeland_return_string = "Homeland \n" + "You Rolled: " + str(roll_value) + ", "
 
-    for x in (homeland.findall('Result')):
+    for x in (homeland_table.findall('Result')):
         if x.find('Name').text == "Unusual Homeland":
-            homeland_return_string += "Unusual! "
+            homeland_return_string += "Unusual Homeland! "
             homeland_return_string += roll_unusual_homeland(race)
             break
         if roll_value <= int(x.attrib['upperBound']):
-
-
             homeland_return_string += x.find('Name').text
             break
     return homeland_return_string
@@ -49,6 +48,19 @@ def roll_unusual_homeland(race=test_race):
 
     return return_string
 
+
+def roll_parents(race=test_race):
+    parents_table = races.find('%s/Parents' % race)
+    die_type = parents_table.attrib['dieType']
+    roll_value=roll(die_type)
+    parents_return_string = "Parents \n" + "You Rolled: " + str(roll_value) + ", "
+
+    for result in parents_table.findall('Result'):
+        if roll_value <= int(result.attrib['upperBound']):
+            parents_return_string += result.find('Name').text
+            break
+
+    return parents_return_string
 
 
 

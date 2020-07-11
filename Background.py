e5e7d5df0ae8b0b8d60787ec1086654316e58a72
Seth <seth5124@Gmail.com>
2020-07-10 21:45:13 -0400

    Sibling data in progress

--- Background.py
+++ Background.py
@@ -4,52 +4,56 @@ import xml.etree.ElementTree as ET
 
 test_race = "Dwarf"
 
-tree = ET.parse('backgroundData.xml')
-root = tree.getroot()
+root = ET.parse('backgroundData.xml').getroot()
 races = root.find('Races')
-misc_tables = root.find('MiscTables')
+
 
 traits = []
 
-def roll(die_type, modifier=0):
-    return random.randint(1, int(die_type)) + modifier
+
+def roll(die_type, modifier=0, multiplier=1):
+
+    return (random.randint(1, int(die_type))*multiplier) + modifier
 
 
 def table_roll(table):
     die_type = table.attrib['dieType']
-    roll_value = roll(die_type)
-    return_string = ""
-
+    modifier = 0
+    multiplier = 1
+    if 'modifier' in table.attrib:
+        modifier = table.attrib['modifier']
+    if 'multiplier' in table.attrib:
+        multiplier = table.attrib['multiplier']
+    roll_value = roll(die_type, modifier, multiplier)
+    return_string = str(roll_value) + ": "
     for result in table.findall('Result'):
 
         roll_upper_bound = result.attrib['upperBound']
         if roll_value <= int(roll_upper_bound):
-            name = str(result.find('Name').text)
-            return_string = str(roll_value) + ": " + name
+            return_string += str(result.find('Name').text)
 
             if 'hasExtraRoll' in result.attrib:
-                extra_roll = result.find('ExtraRoll')
-                extra_roll_table = root.find(extra_roll.find('Table').text)
-                return_string += "\n \t" + table_roll(extra_roll_table)
+                extra_rolls = result.findall('ExtraRoll')
+                for extra_roll in extra_rolls:
+                    table_source = root.find(extra_roll.find('Source').text)
+                    if table_source == "Local":
+                        table_source = extra_roll.find('Table')
+                    return_string += "\n \t" + table_roll(table_source)
             break
-        else:
-            name = result.find('Name').text
-
     return return_string
 
 
-def roll_homeland(race = test_race):
+def roll_homeland(race=test_race):
     print(table_roll(races.find('%s/Homeland' % race)))
 
-def roll_parents(race = test_race):
+
+def roll_parents(race=test_race):
     print(table_roll(races.find('%s/Parents' % race)))
 
 
-
-
 def roll_background(race=test_race):
-    roll_homeland()
-    roll_parents()
+    roll_homeland(race)
+    roll_parents(race)
 
 
 
